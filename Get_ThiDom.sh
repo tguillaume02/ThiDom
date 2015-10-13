b1d0d7cbde6e3397301bfa1d1530e50a3741937d
tguillaume02 <tguillaume@CLT-IT011-FR.a2mac1eu.fr>
2015-10-13 08:15:31 +0200

    install

--- Get_ThiDom.sh
+++ Get_ThiDom.sh
@@ -1,6 +1,1 @@
-sudo aptitude install git
-cd /tmp
-git clone https://github.com/tguillaume02/ThiDom
-cd /tmp/Thidom
-chmod +x Auto_install.sh
-sudo ./Auto_install.sh
+sudo aptitude install git ; cd /tmp  ; git clone https://github.com/tguillaume02/ThiDom ; cd /tmp/ThiDom && chmod +x Auto_install.sh ; sudo ./Auto_install.sh
