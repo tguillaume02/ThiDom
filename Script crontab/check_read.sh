9c06407264058bbe83179a20746c17df2a196f50
tguillaume02 <tguillaume@CLT-IT011-FR.a2mac1eu.fr>
2015-10-13 15:57:26 +0200

    .

--- Script crontab/check_read.sh
+++ Script crontab/check_read.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ `find /var/www/nodejs/public_html/temperature.json -mmin +16` ] &&
+[ `find /home/pi/Script\ crontab/debug/temperature.json -mmin +16` ] &&
 strDate=$(date) &&
 strAct=" Relance read usb" &&
 echo $strDate $strAct >> /home/pi/Script\ crontab/debug/cronNode.txt  &&
