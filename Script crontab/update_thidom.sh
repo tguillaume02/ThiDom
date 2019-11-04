2d0fc97a607eb292ad8da65aaaa2787c5a43f854
tguillaume02 <thib.guillaume@gmail.com>
2019-11-04 19:23:12 +0100

    Fix path

--- Script crontab/update_thidom.sh
+++ Script crontab/update_thidom.sh
@@ -6,11 +6,11 @@ git clone https://github.com/tguillaume02/ThiDom
 cd ThiDom
 sudo rm Script_domotique/msql.py
 sudo rm www/ThiDom/Core/class/db.class.php
-sudo mv Script\ scrontab/update_thidom.sh Script\ scrontab/update_thidom_new.sh
+sudo mv Script\ crontab/update_thidom.sh Script\ crontab/update_thidom_new.sh
 sudo cp -r Script_domotique/ /home/ThiDom/
 sudo cp -r Script\ crontab/ /home/ThiDom/
 sudo cp -r www/ /var/
-sudo mv /home/ThiDom/Script\ scrontab/update_thidom_new.sh /home/ThiDom/Script\ scrontab/update_thidom.sh
+sudo mv /home/ThiDom/Script\ crontab/update_thidom_new.sh /home/ThiDom/Script\ crontab/update_thidom.sh
 
 sudo chmod +x /home/ThiDom/Script\ crontab/*
 sudo chmod 755 /home/ThiDom/Script\ crontab/*
