48d545ef38a4a52ea586c1216e7a6c14ce7b8991
tguillaume02 <thib.guillaume@gmail.com>
2019-11-06 08:29:28 +0100

    Update Get_ThiDom.sh

--- Get_ThiDom.sh
+++ Get_ThiDom.sh
@@ -1,1 +1,1 @@
-sudo apt-get install git -y; cd /tmp  ; git clone https://github.com/tguillaume02/ThiDom ; cd /tmp/ThiDom && chmod +x Auto_install.sh ; sudo ./Auto_install.sh
+sudo apt-get install git -y; cd /tmp  ; git clone -b Dev https://github.com/tguillaume02/ThiDom ; cd /tmp/ThiDom && chmod +x Auto_install.sh ; sudo ./Auto_install.sh
