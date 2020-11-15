3fd972044340b5ceb5fb03baae47887667d0d44f
tguillaume02 <thib.guillaume@gmail.com>
2020-11-15 12:40:25 +0100

    Update Update.sh

--- www/ThiDom/Desktop/Update.sh
+++ www/ThiDom/Desktop/Update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-shell_exec('sudo a2enmod expires');
-shell_exec('sudo a2enmod http2');
-shell_exec('sudo a2enmod headers');
-
+sudo a2enmod expires
+sudo a2enmod http2
+sudo a2enmod headers
